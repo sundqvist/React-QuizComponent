0c4e1f94de67a392619ac6fc1c7425239502c8a1
sundqvist <43414280+sundqvist@users.noreply.github.com>
2020-06-18 15:45:39 +0200

    te
    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import Quiz from './Quiz.js'
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div />
-    )
-  }
+    render() {
+        return (
+            <Quiz />
+        )
+    }
 }
 
 export default App
