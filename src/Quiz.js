5e87a6066934fc9beb1fa7d37a2819a6bd522f43
sundqvist <43414280+sundqvist@users.noreply.github.com>
2020-06-22 11:20:59 +0200

    Added new file QuizQuestion and expanded Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
-//HELLO?
+import QuizQuestion from './QuizQuestion.js'
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { quiz_position: 1 }
-    }
-    render() {
-        return (
-            <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
-        )
-    }
+  constructor(props) {
+    super(props)
+    this.state = { quiz_position: 1 }
+  }
+  render() {
+    return (
+      <div>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      </div>
+    )
+  }
 }
+
 export default Quiz
